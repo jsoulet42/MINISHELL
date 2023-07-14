a85f9584904f8550f6937566f72802d2d308717b
mdiamant <mdiamant@student.42perpignan.fr>
2023-07-14 16:45:43 +0200

    push mdiamant

--- Includes/mdiamant.h
+++ Includes/mdiamant.h
@@ -3,17 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   mdiamant.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lolefevr <lolefevr@student.42perpignan.    +#+  +:+       +#+        */
+/*   By: mdiamant <mdiamant@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 11:28:43 by mdiamant          #+#    #+#             */
-/*   Updated: 2023/07/13 17:23:54 by mdiamant         ###   ########.fr       */
+/*   Updated: 2023/07/14 16:19:09 by mdiamant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-typedef struct s_parsing
+#ifndef MDIAMANT_H
+# define MDIAMANT_H
+
+struct s_parsing
 {
 	char	*str;
-	int		type; //0 = string, 1 = operand, 2 = option, 3 = variable, 4 = num
+	int		type;
+	int		quote_type;
 }				t_par;
 
 int		calc_size_type(char *str);
@@ -21,7 +25,15 @@ int		calc_type(char *str);
 void	sparse(t_par **p, char *argv);
 void	print_t_par(t_par **p);
 int		get_skip_count(const char *str);
-void	ft_parsing(char *argv);
 int		is_quote(char *argv);
 int		verifquote(char *argv);
 void	free_t_par(t_par **p);
+int		is_operand(const char *str);
+int		count_arg(const char *argv);
+t_par	ft_parsing(char *argv);
+
+void	error_exit(char *str);
+
+void	test_dup(t_par **p);
+
+#endif
