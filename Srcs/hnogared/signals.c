3097540df6c34777693ee8429f8b35aebd0b5d7d
Hnogared <hnogared@student.42perpignan.fr>
2023-07-21 13:59:25 +0200

    temp signals

--- Srcs/hnogared/signals.c
+++ Srcs/hnogared/signals.c
@@ -6,7 +6,7 @@
 /*   By: hnogared <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 12:45:13 by hnogared          #+#    #+#             */
-/*   Updated: 2023/07/21 12:00:00 by hnogared         ###   ########.fr       */
+/*   Updated: 2023/07/21 13:59:05 by hnogared         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ void	sig_handler(int signal)
 {
 	if (signal == SIGINT)
 	{
-//		ft_fprintf(STDERR_FILENO, "\n");
+		ft_fprintf(STDERR_FILENO, "\n");
 		rl_replace_line("", 0);
-	//	rl_on_new_line();
+		rl_on_new_line();
 		rl_redisplay();
 	}
 	if (signal == SIGTERM)
