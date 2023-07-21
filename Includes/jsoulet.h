846385fbca135692b45132b94296dd620f6304b0
Julien <illidan888@hotmail.fr>
2023-07-21 17:34:47 +0200

    jsoulet

--- Includes/jsoulet.h
+++ Includes/jsoulet.h
@@ -6,7 +6,7 @@
 /*   By: jsoulet <jsoulet@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 16:26:37 by jsoulet           #+#    #+#             */
-/*   Updated: 2023/07/21 14:39:19 by jsoulet          ###   ########.fr       */
+/*   Updated: 2023/07/21 16:54:19 by jsoulet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@ char	*get_path(char *cmd, t_env *env);
 char	*get_path_cmd(char **path, char *cmd);
 void	piper(t_env *env, t_rinity *cmd_struct);
 void	exec_last(t_env *env, t_rinity *cmd_struct);
-int		create_fd_in(char **file_in, char **type_in, int fd_in);
-int		append_file_content(char *file, int fd_in);
-char	*ft_heredoc(char *str, int fd_in);
+int		create_fd_in(char **file_in, char **type_in);
+int		append_file_content(char *file_in);
+int		ft_heredoc(char *str);
 
 #endif
