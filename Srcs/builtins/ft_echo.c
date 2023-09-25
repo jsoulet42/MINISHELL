051db5f1ff1baa5e38dd9236f90edf82de94ad87
hnogared <hnogared@student.42perpignan.fr>
2023-09-25 15:16:10 +0200

    fixed ft_echo

--- Srcs/builtins/ft_echo.c
+++ Srcs/builtins/ft_echo.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_echo_01.c                                       :+:      :+:    :+:   */
+/*   ft_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jsoulet <jsoulet@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 13:10:12 by jsoulet           #+#    #+#             */
-/*   Updated: 2023/09/04 14:08:44 by hnogared         ###   ########.fr       */
+/*   Updated: 2023/09/25 15:11:59 by hnogared         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../Includes/minishell.h"
 
-static int	index_after_option(char **argv)
+static int	index_after_option(char **argv, int *no_newline)
 {
 	int	i;
 	int	j;
@@ -25,20 +25,23 @@ static int	index_after_option(char **argv)
 		{
 			while (argv[i][j] == 'n')
 				j++;
-			if (argv[i][j] && argv[i][j] != 'n')
-				return (i);
+			if (!ft_is_whitespace(argv[i][j]))
+				return (i - 1);
+			else
+				*no_newline = 1;
 		}
 		else
-			return (i);
+			return (i - 1);
 		i++;
 	}
-	return (0);
+	return (i);
 }
 
 int	ft_echo(int argc, char **argv)
 {
 	int	i;
-	int	string_index;	
+	int	string_index;
+	int	no_newline;
 
 	i = 1;
 	if (argc == 1)
@@ -46,10 +49,11 @@ int	ft_echo(int argc, char **argv)
 		ft_printf("\n");
 		return (0);
 	}
-	string_index = index_after_option(argv);
+	no_newline = 0;
+	string_index = index_after_option(argv, &no_newline);
 	while (argc > 1 && argv[i])
 	{
-		if (i >= string_index)
+		if (i > string_index || (string_index == 0 && !no_newline))
 		{
 			ft_printf("%s", argv[i]);
 			if (i < argc - 1)
@@ -57,7 +61,7 @@ int	ft_echo(int argc, char **argv)
 		}
 		i++;
 	}
-	if (!(argc > 1 && string_index > 1))
+	if (!no_newline)
 		ft_printf("\n");
 	return (0);
 }
